a87c76a8f7f75a911cb2fc12fcbb13a8317e2309
Olexander Osnova <alex.osnova.work@gmail.com>
2022-10-07 19:26:37 +0300

    Update bootstrap
    Move game initialization from bootstrap to index

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,8 +1,6 @@
 import * as env from "./envs.js";
 import DB from "./db/DB.js";
-import InterfaceManager from "./interfaceLogic/InterfaceManager.js";
 
 globalThis["env"] = env;
 globalThis["dbInstance"] = new DB();
 
-new InterfaceManager();
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,6 @@
 import "./bootstrap.js";
+import InterfaceManager from "./interfaceLogic/InterfaceManager.js";
+import scenario from "./scenarios/index.js";
+
+globalThis["scenario"] = scenario;
+new InterfaceManager();
